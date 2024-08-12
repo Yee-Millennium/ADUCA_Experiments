d60edbb360c6604e47eb1b15179ac848fc174b79
Yee <yeewei.math@gmail.com>
2024-08-12 09:30:27 -0500

    Update adapCoder.py

--- src/algorithms/adapCoder.py
+++ src/algorithms/adapCoder.py
@@ -10,88 +10,90 @@ logging.basicConfig(level=logging.INFO)
     
 
 ### This is the version of lambda = 1
-def adapCoder(problem: CompositeFunc, exit_criterion: ExitCriterion, parameters, x_0=None):
+def adapCODER(problem: CompositeFunc, exit_criterion: ExitCriterion, parameters, x_0=None):
     # Init of adapCODER
     L = parameters["L"]
     beta1, beta2, beta3 = parameters["beta1"], parameters["beta2"], parameters["beta3"]
-    rho_1 = (1 + beta1 * beta3 /(1-beta3)) * beta3
-    rho_2 = (1-beta2)**2 / beta3**2
-    a = np.ones(4)
-    A = 0
+    rho_1 = (1 + (beta1 * beta3)/(1-beta3)) * beta3
+    rho_2 = (1-beta2) / beta3
+    rho_3 = (1-beta2) / beta1 * beta2 * 10
+    rho_4 = ((beta1 * beta2 *(1-beta2)) / ((1-beta3)*beta3)) **0.5
+
+    theta = np.ones(3)
 
     if x_0 is None:
-        x_0 = np.zeros(problem.d)
-    x_1 = np.copy(x_0)
-    x_1[0] = 1
-    count = 0
+        x_0 = np.random.randn(problem.d)
+    perturbation = np.random.uniform(-0.0001, 0.0001, size=x_0.shape)
+    x_1 = x_0 + perturbation
     while np.array_equal( problem.grad(x_0),  problem.grad(x_1)):
-        count += 1
-        x_1[count] = 1 
+        perturbation = np.random.uniform(-0.0001, 0.0001, size=x_0.shape)
+        x_1 += perturbation
+
     x = np.zeros((4, problem.d))
     x[-2], x[-1] = x_0, x_1
-    x_sum = np.zeros(problem.d)
-    x_hat = np.zeros(problem.d)
-    v = np.zeros(problem.d)
-    v_minus_1 = np.copy(v)
+
+    v = np.copy(x[-1])
+    v_ = np.copy(v)
 
     loss_grad = np.zeros((3, problem.d))
-    loss_grad[-3], loss_grad[-2], loss_grad[-1] = problem.loss_func.grad(x_0), problem.loss_func.grad(x_0), problem.loss_func.grad(x_1)
-    p= np.copy(loss_grad)
-    q = problem.loss_func.grad(x_0)
+    loss_grad[-2], loss_grad[-1] = problem.loss_func.grad(x_0), problem.loss_func.grad(x_1)
 
     b_A_x = np.zeros(problem.loss_func.n)
     m = problem.d  # Assume that each block is simply a coordinate for now
 
+    p= np.zeros_like(loss_grad)
+    for j in range(0, m, 1):
+        if j == 0:
+            loss_func_grad_x, b_A_x = problem.loss_func.grad_block_update(x[-2])
+            p[-1][0] = loss_func_grad_x[0]
+        else:
+            p[-1][j], b_A_x = problem.loss_func.grad_block_update(b_A_x, (j-1, x[-1][j-1] - x[-2][j-1]), j)
+    p[-2] = np.copy(loss_grad[-2])
+
+    q = np.zeros(problem.d)
+
+    step = np.linalg.norm(x[-1] - x[-2]) / np.linalg.norm(loss_grad[-1] - loss_grad[-2])
+    a = step
+    a_ = step
+    A = 0
+
+    theta = np.ones(3)
+
 
     # Stepsize selection function
-    def adapCoder_stepsize(x, a_step, grad, tildeGrad):
-        step_vec = np.zeros(6)
-        step_1 = rho_1 * a_step[-1]
-        step_vec[0] = step_1
+    def adapCoder_stepsize(x, a, a_, theta, grad, tildeGrad, iter=3):
+        # step_1 = rho_1 * a
+        step_1 = rho_1 * a * 1.2
 
-        delta_x = x[-1] - x[-2]
-        delta_grad = grad[-1] - grad[-2]
-        if np.any(delta_grad):
-            inv_Lsquare = (delta_x @ delta_x) / (delta_grad @ delta_grad)
-            step_2 = rho_2 / (300 * a_step[-2] ) * inv_Lsquare
-            step_vec[1] = step_2
-        else:
-            step_vec[1] = step_1
-        
-        delta_x_minus_1 = x[-2] - x[-3]
-        delta_tildeGrad_minus_1 = grad[-2] - tildeGrad[-2]
-        if np.any(delta_tildeGrad_minus_1):
-            invHat_Lsquare_minus_1 =  (delta_x_minus_1 @ delta_x_minus_1) / (delta_tildeGrad_minus_1 @ delta_tildeGrad_minus_1)
-            step_3 = rho_2 * a_step[-2] / (300 * a_step[-3] * a_step[-1]) * invHat_Lsquare_minus_1
-            step_vec[2] = step_3
-        else:
-            step_vec[2] = step_1
-        
-        step_4 = (a_step[-1]**2 *(1-beta2)) / (10 * a_step[-2] * beta1 * beta2)
-        step_vec[3] = step_4
+        ### we can heuristically decrease the constant on the denominator
+        L_k = np.linalg.norm(grad[-1] - grad[-2]) / np.linalg.norm(x[-1] - x[-2])
+        # step_2 = (rho_2**2 * theta[-1]) / (300 * a * L_k**2)
+        step_2 = (rho_2**2 * theta[-1]) / (50 * a * L_k**2)
 
-        delta_tildeGrad = grad[-1] - tildeGrad[-1]
-        if np.any(delta_tildeGrad):
-            invHat_L = ((delta_x @ delta_x) / (delta_tildeGrad @ delta_tildeGrad))**0.5
-            step_5 = 1/invHat_L * ((a_step[-1]*beta1*beta2*(1-beta2)) / (10*a_step[-2]*beta3*(1-beta3))) **0.5
-            step_vec[4] = step_5
+        if iter <= 1:
+            step_3 = 10000
         else:
-            step_vec[4] = step_1
-
-        delta_x_minus_2 = x[-3] - x[-4]
-        delta_tildeGrad_minus_2 = grad[-3] - tildeGrad[-3]
-        if np.any(delta_tildeGrad_minus_2):
-            invHat_Lsquare_minus_2 = (delta_x_minus_2 @ delta_x_minus_2) / (delta_tildeGrad_minus_2 @ delta_tildeGrad_minus_2)
-            step_6 = rho_2 * (  (a_step[-3] * a_step[-1]) / (60 * a_step[-4] *a_step[-2]**2) ) * invHat_Lsquare_minus_2
-            step_vec[5] = step_6
-        else:
-            step_vec[5] = step_1
+            L_hat_ = np.linalg.norm(tildeGrad[-2] - grad[-2]) / np.linalg.norm (x[-2] - x[-3])
+            # step_3 = (rho_2**2 * theta[-2]) / (300 * a * L_hat_**2)
+            step_3 = (rho_2**2 * theta[-2]) / (50 * a * L_hat_**2)
         
-        min_step = min(step_vec)
-        if min_step > 1/L:
-            return min_step
+        # step_4 = rho_3 * theta[-1] * a
+        step_4 = rho_3 * theta[-1] * a * 1.5
+
+        L_hat = np.linalg.norm(tildeGrad[-1] - grad[-1]) / np.linalg.norm(x[-1] - x[-2])
+        # step_5 = rho_4 * (theta[-1] / 10)**0.5 / L_hat
+        step_5 = rho_4 * (theta[-1] / 5)**0.5 / L_hat
+
+        if iter <= 2:
+            step_6 = 10000
         else:
-            return 1/L
+            L_hat__ = np.linalg.norm(tildeGrad[-3] - grad[-3]) / np.linalg.norm(x[-3] - x[-4])
+            # step_6 = (rho_2**2 * theta[-1] * theta[-3]) / (60 * a_ * L_hat__**2)
+            step_6 = (rho_2**2 * theta[-1] * theta[-3]) / (10 * a_ * L_hat__**2)
+        
+        step = min(step_1, step_2, step_3, step_4, step_5, step_6)
+        # print(f" !!! Stepsize: {step}")
+        return step
 
 
 
@@ -104,34 +106,44 @@ def adapCoder(problem: CompositeFunc, exit_criterion: ExitCriterion, parameters,
     logresult(results, 1, 0.0, init_opt_measure)
 
     while not exit_flag:
-        np.copyto(v_minus_1, v)
-        
+        np.copyto(v_, v)
+
         # Step 4
-        step = adapCoder_stepsize(x, a, loss_grad, p)
-        a = np.append(a[1:], step)
+        if iteration == 0:
+            step = adapCoder_stepsize(x, a, a_, theta, loss_grad, p, iter=1)
+        elif iteration == 1:
+            step = adapCoder_stepsize(x, a, a_, theta, loss_grad, p, iter=2)
+        else:
+            step = adapCoder_stepsize(x, a, a_, theta, loss_grad, p)
+        a_ = a
+        a = step
+        theta = np.append(theta[1:], a  / a_)
 
         # Step 5
+        p = np.append(p[1:], np.zeros((1, problem.d)), axis=0)
         for j in range(0, m):
-            # Step 6
-            if j == 0:
-                p = np.append(p[1:], np.array([loss_grad[-2]]), axis=0)
-            else:
-                p[-1][j], b_A_x = problem.loss_func.grad_block_update(b_A_x, (j-1, x[-1][j-1] - x[-2][j-1]), j)
-
             # Step 7
-            q[j] = p[-1][j] + (a[-2] / a[-1]) * (loss_grad[-2][j] - p[-2][j])
+            q[j] = p[-2][j] + (a_ / a) * (loss_grad[-2][j] - p[-3][j])
             
             # Step 8
             if j == 0:
-                z_0 = problem.reg_func.prox_opr_block(v_minus_1[j] - step * q[j], step)
+                z_0 = problem.reg_func.prox_opr_block(v_[j] - a * q[j], a)
                 x = np.append(x[1:], np.zeros((1, problem.d)), axis=0) 
                 x[-1][0] = z_0
             else:
-                x[-1][j] = problem.reg_func.prox_opr_block(v_minus_1[j] - step * q[j], step)
+                x[-1][j] = problem.reg_func.prox_opr_block(v_[j] - a * q[j], a)
 
             # Step 9
-            v[j] = beta1 * x[-1][j] + beta2 * x[-2][j] + beta3 * v_minus_1[j]
+            v[j] = beta1 * x[-1][j] + beta2 * x[-2][j] + beta3 * v_[j]
             
+            # Step 10
+            if j == 0:
+                loss_func_grad_x, b_A_x = problem.loss_func.grad_block_update(x[-2])
+                p[-1][0] = loss_func_grad_x[0]
+            else:
+                p[-1][j], b_A_x = problem.loss_func.grad_block_update(b_A_x, (j-1, x[-1][j-1] - x[-2][j-1]), j)
+
+
         loss_grad = np.append(loss_grad[1:], np.array([problem.loss_func.grad(x[-1])]), axis=0)
 
         ### Compute x_hat
